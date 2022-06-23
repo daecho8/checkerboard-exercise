e516fc6f5c4ebd52a8e1caea0883ad9c544c45ac
Daeyeon Cho <107440788+daecho8@users.noreply.github.com>
2022-06-23 17:07:18 -0500

    Normal Checkbaord finished

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Your JS goes here
+
+const container = document.createElement('div');
+document.body.append(container)
+for (var i = 1; i <= 8; i++) {
+    for (var j = 1; j <= 8; j++) {
+        var div = document.createElement('div')
+        div.style.float = 'left'; // also possible with flexbox works (extra later)
+        div.style.width = '11.1%';
+        div.style.paddingBottom = '11.1%'
+        div.style.border = '1px solid black'
+        if (i % 2 === 1) {
+            if (j % 2 === 1) {
+                div.style.background = 'black';
+            } else {
+                div.style.background = 'red';
+            }            
+        } else {
+            if (j % 2 === 1) {
+                div.style.background = 'red';
+            } else {
+                div.style.background = 'black';
+            }
+        } 
+        container.append(div);
+    }
+}
